018f949fb95f624497dc1959bcaaf59de60f35a5
t-hase-prog <b2221840@photon.chitose.ac.jp>
2023-11-22 07:35:50 +0900

    学生情報テーブルを作成した

--- prmn2023b/src/main/java/lecture02/pra2_2.java
+++ prmn2023b/src/main/java/lecture02/pra2_2.java
@@ -11,25 +11,6 @@ public class pra2_2 {
         String name_2 = scan.next();
         System.out.print("学籍番号を入力して下さい。");
         int num_2 = scan.nextInt();
-        String message = log.sys(name_2, num_2);
-        message.mes();
-    }
-
-    public Login() {
-        String name;
-        int num;
-        Login(String name, int num){
-            this.name = name;
-            this.num = num;
-        }
-        void message (String name_n ,int num_n){
-            if (name.equals(name_n)) {
-                if (num == num_n) {
-                    System.out.println("ログイン成功。");
-                }
-            } else {
-                System.out.println("ログイン失敗。");
-            }
-        }
+        log.message(name_2, num_2);
     }
 }
